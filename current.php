40eab3a14c242fbbab1ee34ff4488f9c32c85900
Michelle Brownlee <michelle@orbwave.com>
2021-01-05 11:42:53 -0600

    playing with real loop

--- current.php
+++ current.php
@@ -1,32 +1,6 @@
 <?php
 
-$db = new mysqli("ipaddress","teammates","password","teammates");
 
-//$time_start = microtime(true);
-
-$input = "";// text to encrypt
-$bit_check=128;// bit amount for diff algor.
-
-function decrypt($encrypted_text,$key,$iv,$bit_check){
-
-$last_char=substr($decrypted,-1);
-for($i=0;$i<$bit_check-1; $i++){
-if(chr($i)==$last_char){
-$decrypted=substr($decrypted,0,strlen($decrypted)-$i);
-break;
-}
-}
-return $decrypted;
-}
-
-function encrypt($text,$key,$iv,$bit_check) {
-$text_num =str_split($text,$bit_check);
-$text_num = $bit_check-strlen($text_num[count($text_num)-1]);
-for ($i=0;$i<$text_num; $i++) {$text = $text . chr($text_num);}
-
-}
-
-//Make a loop to encrypt the data
 
 $query = $db->query("SELECT * FROM tempW2 WHERE done=0 LIMIT 200");
 
@@ -38,4 +12,4 @@ $db->query("UPDATE tempW2 SET SSN='".encrypt($data['SSN'],$key,$iv,$bit_check)."
 
 $db->close();
 
-?> 
+?>
--- index.php
+++ index.php
@@ -7,18 +7,55 @@
     <?php
     include_once 'db_sql.php';
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $sql = "SELECT * FROM pretendw2 WHERE done=0 LIMIT 5;";
-    $result = mysqli_query($init, $sql);
+    // $sql = "SELECT * FROM pretendw2 WHERE done=0 LIMIT 5;";
+    // $result = mysqli_query($init, $sql);
     // $array_size = $result;
     // for ($i=0; $i<$array_size; $i++) {
-        while ($row = $result->fetch_assoc()) {
-            echo $row['name'] . " " . $row['id'] . "</br>";
-            $init->$sql("UPDATE pretendw2 SET done=1;");
-            if there are more records, do this again.
-    
-}
-}
-    
+        // while ($row = $result->fetch_assoc()) {
+        //     echo $row['name'] . " " . $row['id'] . "</br>";
+        //     $init->$sql("UPDATE pretendw2 SET done=1;");
+        //    $query1 = Select crap
+        //    if $query1 returns crap then
+        //    do {$query2 = select 200 records
+        //     while(loop $query2)} {
+        //         encrypt each line
+        //     } $query3 checks for more crap and
+        //     while go through again if $query3 returns stuff
+        // $query = $db->query(
+        //     do {
+        //     $sql = "SELECT name, id FROM pretendw2 LIMIT 5;";
+        //     $result = mysqli_query($init, $sql);
+        //     // $data = $result->fetch_array();
+            
+        //     while ($data = $result->fetch_array()) {
+        //         echo $data['name'] . " " . $data['id'] . "</br>";
+        //     }
+        // } while ($data !== null);
+        
+        // do {
+        //     $sql = "SELECT name, id, done FROM pretendw2 WHERE done = 0 LIMIT 5;";
+        //     $result = mysqli_query($init, $sql);
+        //     $num_rows = mysqli_num_rows($result);
+        //     if ($num_rows > 0){
+        //         while ($row=$result -> fetch_array()) {
+        //             $finished = $row['id'];
+        //             $sql = "UPDATE pretendw2 SET done = 1 WHERE id = $finished;";
+        //             echo $row['name'] . "</br>";
+        //         } 
+        //     } } while ($num_rows > 0);
+            }
+        do{
+            $query = $db->query("SELECT * FROM tempW2 WHERE done=0 LIMIT 200");
+            $result = mysqli_query($init, $sql);
+            $num_rows = mysqli_num_rows($result);
+            if ($num_rows > 0){
+                while($row = $result->fetch_array()){
+                $unencryptSSN = $data['SSN'];
+                $year = $data['W2Year'];
+                $db->query("UPDATE tempW2 SET SSN='".encrypt($data['SSN'],$key,$iv,$bit_check)."', done=1 WHERE SSN='$unencryptSSN'");
+            } } } while ($num_rows > 0);
+            
+
 
     ?>
 </body>
